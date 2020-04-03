563da91bc1baf05c81905d056bfc0332bb56146d
Federico Paolinelli <fpaoline@redhat.com>
2020-04-03 16:18:05 +0200

    Add fpaoline to sudoers
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- setup.sh
+++ setup.sh
@@ -21,9 +21,10 @@ systemctl reload NetworkManager
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-
+mv ./kubectl /usr/local/bin/kubectl
 useradd -m fpaoline
+gpasswd wheel -a fpaoline
+
 chmod 600 fede.pub
 mv fede.pub ~fpaoline
 
